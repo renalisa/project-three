ae3ef8529e7fc43ff45ba50d36fb4be9910756a1
renalisa <renalisa@users.noreply.github.com>
2021-11-17 23:47:15 +0000

    deploy: 0c30f5930f2c176000a60a6b5cb164a96231b118

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+/* TRYING TO GET INPUT TEXT TO SHOW UP */
 
+function getVal() {
+  //create li element
+  let li = document.createElement("li");
+
+  //put text incisde the p element
+  li.innerText = "input";
+
+  //append the li element as a child of the pages body
+  document.body.appendChild(li);
+}
+
+function getVal() {
+  const value = document.querySelector("input").value;
+  console.log(val);
+}
