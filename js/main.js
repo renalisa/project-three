ab1f599d0eff64441906e414f244a47d19901b7f
renalisa <renalisa@users.noreply.github.com>
2021-11-22 05:42:12 +0000

    deploy: 5339605579c43286a6f71ca678f17d84956d5aae

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+window.addEventListener("load", () => {
+  let mainDiv = document.getElementsByClassName("scrollbar")[0];
+
+  let card1 = document.createElement("div");
+  let card2 = document.createElement("div");
+  let card3 = document.createElement("div");
+
+  card1.addEventListener("click", () => {});
+  card2.addEventListener("click", () => {});
+  card3.addEventListener("click", () => {});
+
+  let popup1 = document.createElement("div");
+  let popup2 = document.createElement("div");
+  let popup3 = document.createElement("div");
+
+  /*
+  popup1.onclick = function() {
+    modal.style.display = "block";
+  popup2.onclick = function() {
+    modal.style.display = "block";
+  popup3.onclick = function() {
+    modal.style.display = "block";
+
+  span.onclick = function() {
+      modal.style.display = "none";
+*/
+
+  card1.innerText = "hey whats up";
+  card2.innerText = "hi this is renata";
+  card3.innerText = "cool";
+
+  mainDiv.appendChild(card1);
+  mainDiv.appendChild(card2);
+  mainDiv.appendChild(card3);
+});
+
 var locations = document.querySelector("#locations");
 
 /* TRYING TO GET INPUT TEXT TO SHOW UP */
@@ -22,25 +58,27 @@ document.querySelector("input").addEventListener("keypress", function (e) {
   }
 });
 
-
 // destination boxes //
 //functon//
 
 let myCard = {
   title: "my trip",
-  blurb: "fhakjhakfhna"
+  blurb: "fhakjhakfhna",
+};
+
+function getTravelCardHTML(card, title) {
+  let div = document.createElement("div");
+  // let template = '<article>
+  //   <span class="span">x</span>
+  //   <h2>${card.title}</h2>
+  //   <img>${card.img}</img>
+
+  //   </article>';
+
+  dive.innerHTML = template;
+
+  return template;
 }
+// div.document.body.appendChild(getTravelCardHTML)(myCard);
 
-function getTravelCardHTML(card, title){
-  let dive = document.createElement("div");
-let template = '<article>
-  <h2>${card.title}</h2>
-
-  </article>';
-
-  dive.innerHTML = template; 
-
-return template;
-
-}
-document.body.appendChild(getTravelCardHTML)(myCard)
+document.getElementsByClassName(".scrollbar")[0];
