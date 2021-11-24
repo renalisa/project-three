5af7f379f7ec79cc921b333ba70f4674882c28a1
RENATA SCHMIDT <renalisa@live.unc.edu>
2021-11-24 11:56:54 -0800

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,84 +1,74 @@
 window.addEventListener("load", () => {
-  let mainDiv = document.getElementsByClassName("scrollbar")[0];
+  let arr = [];
 
-  let card1 = document.createElement("div");
-  let card2 = document.createElement("div");
-  let card3 = document.createElement("div");
+  let select = document.querySelector("select");
+  select.addEventListener("change", function () {
+    console.log(select.value);
 
-  card1.addEventListener("click", () => {});
-  card2.addEventListener("click", () => {});
-  card3.addEventListener("click", () => {});
+    renderCards(filterRegions(arr, select.value));
+  });
 
-  let popup1 = document.createElement("div");
-  let popup2 = document.createElement("div");
-  let popup3 = document.createElement("div");
+  getData();
 
-  /*
-  popup1.onclick = function() {
-    modal.style.display = "block";
-  popup2.onclick = function() {
-    modal.style.display = "block";
-  popup3.onclick = function() {
-    modal.style.display = "block";
+  function renderCards(cards) {
+    document.querySelector(".scrollbar").innerHTML = "";
+    cards.forEach((card) => {
+      document.querySelector(".scrollbar").appendChild(getTravelCardHTML(card));
+    });
+  }
 
-  span.onclick = function() {
-      modal.style.display = "none";
-*/
+  // GET API DATA
+  async function getData() {
+    const response = await fetch("./js/main.json");
+    const data = await response.json();
+    arr = data;
+    renderCards(data);
+  }
 
-  card1.innerText = "hey whats up";
-  card2.innerText = "hi this is renata";
-  card3.innerText = "cool";
+  function filterRegions(regions, term) {
+    if (term === "Any") return regions;
+    return regions.filter((i) => i.continent === term);
+  }
 
-  mainDiv.appendChild(card1);
-  mainDiv.appendChild(card2);
-  mainDiv.appendChild(card3);
-});
+  var locations = document.querySelector("#locations");
 
-var locations = document.querySelector("#locations");
+  function getVal() {
+    //create li element
+    let li = document.createElement("li");
 
-/* TRYING TO GET INPUT TEXT TO SHOW UP */
+    //put text incisde the p element
+    li.innerText = document.querySelector("input").value;
 
-function getVal() {
-  //create li element
-  let li = document.createElement("li");
+    //append the li element as a child of the pages body
+    locations.appendChild(li);
+    document.querySelector("input").value = "";
+  }
 
-  //put text incisde the p element
-  li.innerText = document.querySelector("input").value;
+  document.querySelector("input").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      getVal();
+    }
+  });
 
-  //append the li element as a child of the pages body
-  locations.appendChild(li);
-  document.querySelector("input").value = "";
-}
-// make global variable that
+  let myCard = {
+    title: "my trip",
+    blurb: "fhakjhakfhna",
+  };
 
-// Add event listener for entering
-document.querySelector("input").addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    getVal();
+  function getTravelCardHTML(card) {
+    let div = document.createElement("div");
+    let template = `<div>
+   <span class="span">x</span>
+  <h3>${card.destination}</h3>
+  <p>${card.city}</p>
+  <p>${card.country}</p>
+  <p>${card.continent}</p>
+  <p>${card.space}</p>
+      
+    </div>`;
+
+    div.innerHTML = template;
+
+    return div;
   }
 });
-
-// destination boxes //
-//functon//
-
-let myCard = {
-  title: "my trip",
-  blurb: "fhakjhakfhna",
-};
-
-function getTravelCardHTML(card, title) {
-  let div = document.createElement("div");
-  // let template = '<article>
-  //   <span class="span">x</span>
-  //   <h2>${card.title}</h2>
-  //   <img>${card.img}</img>
-
-  //   </article>';
-
-  dive.innerHTML = template;
-
-  return template;
-}
-// div.document.body.appendChild(getTravelCardHTML)(myCard);
-
-document.getElementsByClassName(".scrollbar")[0];
